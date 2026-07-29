c7ba70614554ae1e5ad83288e189f6993e612956
Quentin Stehn <157679418+QJS17@users.noreply.github.com>
2026-07-29 14:23:20 +0200

    Initial Commit

---
+++ src/main/java/Praktikum/CampusRallyeCode.java
@@ -0,0 +1,44 @@
+public class CampusRallyeCode {
+    public static void main(String[] args) {
+        
+
+
+
+
+
+
+
+        String ergebnis1 = ("Glückwunsch, ihr als Team " + teamName + " habt " + gefundenePflanzen + " von 12 Pflanzen gefunden. \n");
+
+        String ergebnis2;
+        if (ballWechselGeschafft) {
+            ergebnis2 = ("Außerdem habt ihr alle 10 Ballwechel geschaft. \n");
+        } else {
+            ergebnis2 = ("Leider habt ihr nicht alle 10 Ballwechel geschaft. \n");
+        }
+        String ergebnis3;
+        if (inPongGewonnen) {
+            ergebnis3 = ("Zudem habt ihr auch bei Pong gewonnen. \n");
+        } else {
+            ergebnis3 = ("Im Pong habt ihr jedoch ganz knapp verloren. \n");
+        }
+
+        String ergebnis4;
+        if (summeGebaeudenummern == 523) {
+            ergebnis4 = ("Beim Gebäudenummern zudammenzählen seit ihr auf " + summeGebaeudenummern + " gekommen, das stimmt! \n");
+        } else {
+            ergebnis4 = ("Beim Gebäudenummern zudammenzählen seit ihr auf " + summeGebaeudenummern + " gekommen, das ist leider falsch. \n");
+        }
+
+        String ergebnis5;
+        if (distanzPapierflieger > 4.0) {
+            ergebnis5 = ("Euer Papierflieger ist " + distanzPapierflieger + " Meter weit geflogen, sehr gut. \n");
+        } else {
+            ergebnis5 = ("Euer Papierflieger ist " + distanzPapierflieger + " Meter weit geflogen, das geht besser. \n");
+        }
+
+        String ergebnis6 = ("Beim Atruvia-Quiz habt ihr " + richtigeQuizAntworten + " von 3 richtigen Antworten gelifert. \n");
+
+        System.out.println(ergebnis1 + ergebnis2 + ergebnis3 + ergebnis4 + ergebnis5 + ergebnis6 + "Wir hoffen dass ihr Spaß hattet und das euch der Campus gefallen hat.");
+    }
+}
